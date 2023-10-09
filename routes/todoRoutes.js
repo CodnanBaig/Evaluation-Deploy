6d34ab39f6bf1d2188dd2c3f437b9bc783cdf9ff
CodnanBaig <adnanbaigofficial@gmail.com>
2023-10-09 23:01:25 +0530

    Update

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,7 +4,7 @@ const verifyToken = require("../middlewares/verifyToken");
 const todoRouter = require("express").Router();
 
 todoRouter.post("/todos", verifyToken, addTodo);
-todoRouter.get("/todos", verifyToken, getUserTodos);
+todoRouter.get("/todos", getUserTodos);
 todoRouter.get("/todos/:id", verifyToken, getSingleTodo);
 todoRouter.delete("/todos/:id", verifyToken, deleteTodo)
 todoRouter.put("/todos/:id",verifyToken, updateTodo);
